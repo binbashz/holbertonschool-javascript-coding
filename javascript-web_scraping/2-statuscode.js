194daeb71712675afc4ec0e55cd5eb5ee10dbb67
binbashz <5732@holbertonstudents.com>
2023-09-22 10:03:13 -0700

    delete blanks

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-
 // Get the URL from the command line arguments
 const url = process.argv[2];
-
 // Send a GET request to the specified URL
 request(url, (error, response, body) => {
   if (error) {
