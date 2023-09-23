e7e95b3c7c3d5e78f2f05e9652870b57d5ddba63
binbashz <5732@holbertonstudents.com>
2023-09-23 16:42:25 -0700

    semistandar

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
-const characterId = '18';
 
-request(apiUrl, function (error, response, body) {
+// Send a GET request to the Star Wars API
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const count = films.filter((film) => film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)).length;
-    console.log(count);
   }
+
+  // Convert the JSON object (key-value pairs) into a Javascript object
+  const data = JSON.parse(body);
+
+  let numberOfMovies = 0;
+
+  // Iterate through each movie
+  data.results.forEach((movie) => {
+    // Iterate through each character in the movie
+    movie.characters.forEach((character) => {
+      // Check if the character URL includes the ID 18
+      if (character.includes('/people/18/')) {
+        // If found, increment the movie counter
+        numberOfMovies++;
+      }
+    });
+  });
+  // Print the number of movies
+  console.log(numberOfMovies);
 });
