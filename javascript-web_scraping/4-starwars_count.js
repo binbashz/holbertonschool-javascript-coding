7e213fc37d9f2385f3b273c88cdd13ae0a6096cb
binbashz <5732@holbertonstudents.com>
2023-09-23 16:44:16 -0700

    4-starwars_count.js

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const api_Url = process.argv[2];
+const characterId = '18';
+
+request(api_Url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body);
+    const count = films.results.filter((film) => film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)).length;
+    console.log(count);
+  }
+});
+
